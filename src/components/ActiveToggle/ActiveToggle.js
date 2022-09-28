d38608d45a1f3fbee99e595c62edd03eb7ebb131
Patrick <pamarx92@gmail.com>
2022-09-28 11:41:47 +0200

    added useState

--- src/components/ActiveToggle/ActiveToggle.js
+++ src/components/ActiveToggle/ActiveToggle.js
@@ -1,10 +1,17 @@
 import "./ActiveToggle.css";
+import { useState } from "react";
+
+
 
 function ActiveToggle() {
+  const [active, setState] = useState("box");
+  const box = document.querySelector(".box");
+
   return (
+
     <main>
       <div className="box" />
-      <button type="button">Activate</button>
+      <button type="button" onClick={() => setState(box.classList.toggle("box--active"))}>Activate</button>
     </main>
   );
 }
--- src/components/ShoppingItem/ShoppingItem.js
+++ src/components/ShoppingItem/ShoppingItem.js
@@ -1,7 +1,9 @@
 import "./ShoppingItem.css";
+import { useState } from "react"
 
 function ShoppingItem({ name, price }) {
-  const amount = 1;
+  const [amount, setAmount] = useState(0);
+
 
   return (
     <section className="item">
@@ -9,10 +11,10 @@ function ShoppingItem({ name, price }) {
       <p>price: {price}</p>
       <p>amount: {amount}</p>
       <p>total: {price * amount}</p>
-      <button className="item__button" type="button">
+      <button className="item__button" type="button" onClick={() => setAmount(amount - 1)}>
         -
       </button>
-      <button className="item__button" type="button">
+      <button className="item__button" type="button" onClick={() => setAmount(amount + 1)}>
         +
       </button>
     </section>
